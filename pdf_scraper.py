2bb5b017f5256f1765124540707038151ce5084e
Elliott Bartsch <elliott@firecracker.me>
2013-08-17 17:58:26 -0400

    Pdf email link scraper

---
+++ pdf_scraper.py
@@ -0,0 +1,30 @@
+import pyPdf
+
+f = open('Student-Activities-Guide-Final-2011-2012.pdf','rb')
+
+pdf = pyPdf.PdfFileReader(f)
+pgs = pdf.getNumPages()
+key = '/Annots'
+uri = '/URI'
+ank = '/A'
+
+emails = []
+
+for pg in range(pgs):
+
+    p = pdf.getPage(pg)
+    o = p.getObject()
+
+    if o.has_key(key):
+        ann = o[key]
+        for a in ann:
+            u = a.getObject()
+            if u[ank].has_key(uri) and 'mailto:' in u[ank][uri]:
+                email = u[ank][uri].replace('mailto:', '')
+                if email not in emails:
+                	emails.append(email)
+
+for email in emails:
+	print email
+
+f.close()
